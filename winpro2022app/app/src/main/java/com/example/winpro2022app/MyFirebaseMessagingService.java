5bcde7c4f45d5878c32cc861655acb6bf0229cba
GomLV <ysj97@naver.com>
2022-01-13 00:01:54 +0900

    중간저장 2022 01 12
    중간저장. 현재 CHANELID 가 무엇인지 찾아야함

---
+++ winpro2022app/app/src/main/java/com/example/winpro2022app/MyFirebaseMessagingService.java
@@ -0,0 +1,60 @@
+package com.example.winpro2022app;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat; //이부분이 원래는 import android.support.v4.app.NotificationCompat; 임
+import androidx.core.app.NotificationManagerCompat;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFireBaseMessagingService extends FirebaseMessagingService {
+    String token = FirebaseMessaging.getInstance().getToken().getResult();
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        //token을 서버로 전송
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        //수신한 메시지를 처리
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel);
+            }
+            builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+        }else {
+            builder = new NotificationCompat.Builder(getApplicationContext());
+        }
+
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        builder.setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
+        Notification notification = builder.build();
+        notificationManager.notify(1, notification);
+    }
+    }
+}
